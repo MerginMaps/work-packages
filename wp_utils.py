6a4953e0532a49c501e0716087e7851a5d3c0d6d
lukasz.debek <lukasz.debek@lutraconsulting.co.uk>
2021-09-24 13:56:35 +0200

    Reworking caching of the project files.

--- wp_utils.py
+++ wp_utils.py
@@ -3,7 +3,6 @@ Module with useful utilities.
 """
 import os
 import shutil
-import string
 
 
 def escape_double_quotes(name):
@@ -16,14 +15,11 @@ def download_project_with_cache(mc, project_path, directory, cache_dir, version=
     if not cache_dir:
         mc.download_project(project_path, directory, version=version)
         return
-    string_translation = str.maketrans("", "", string.punctuation)
-    project_info = mc.project_info(project_path)
-    updated_timestamp = project_info["updated"]
-    project_name = project_path.split("/")[-1]
-    translated_timestamp = updated_timestamp.translate(string_translation)
-    project_cache_dir = os.path.join(cache_dir, f"{project_name}_{translated_timestamp}")
+    project_namespace, project_name = project_path.split("/")
+    project_cache_dir = os.path.join(cache_dir, f"{project_namespace}_{project_name}")
     if not os.path.exists(project_cache_dir):
         mc.download_project(project_path, project_cache_dir, version=version)
     else:
-        print("Project already checked out - copying cached files")
+        mc.pull_project(project_cache_dir)
+        print("Project cached - copying existing files")
     shutil.copytree(project_cache_dir, directory)
