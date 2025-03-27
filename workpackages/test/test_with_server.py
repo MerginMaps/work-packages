2e495e77a0180bc7a054040540b6ead422b73396
lukasz.debek <ldebek@interia.pl>
2025-03-27 17:57:36 +0100

    Changes in test_with_server.py

--- workpackages/test/test_with_server.py
+++ workpackages/test/test_with_server.py
@@ -20,7 +20,7 @@ this_dir = os.path.dirname(os.path.realpath(__file__))
 
 
 def create_client(user, pwd):
-    assert SERVER_URL and SERVER_URL.rstrip("/") != "https://app.dev.merginmaps.com" and user and pwd
+    assert SERVER_URL and API_USER and USER_PWD
     return MerginClient(SERVER_URL, login=user, password=pwd)
 
 
